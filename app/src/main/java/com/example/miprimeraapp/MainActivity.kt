7ef3e601c757ea8154593f15d35cc369fcc7e28c
Mario Javier Oñate Aldaz <Mariodatos@outlook.com>
2021-09-20 12:37:31 +0200

    Idioma pantalla de pie/tumbada

--- app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //comentario 2
-    }
 
+        Log.v("MainActivity", "Se ha creado un texto corrctamente")
+        Toast.makeText(this,"Hola",Toast.LENGTH_LONG).show()
+
+
+
+
+    }
 }
 
 
